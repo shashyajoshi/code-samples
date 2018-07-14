f40959a81b20adfec061064227f5563d9e97fd24
Shashank <shashank@localhost.localdomain>
2018-07-14 14:52:15 -0500

    Additional comments

--- gcloud-python-api/get_instance_data_api.py
+++ gcloud-python-api/get_instance_data_api.py
@@ -2,10 +2,12 @@ import googleapiclient.discovery
 from oauth2client.client import GoogleCredentials
 
 def get_billing_id(cloud_billing_client):
+    # Get billing account
     billing_result = cloud_billing_client.billingAccounts().list().execute()
     return billing_result["billingAccounts"][0]['name']
 
 def get_project_list(cloud_billing_client,billing_id):
+    # Get list of all projects in the billing id
     project_list = []
     project_result = cloud_billing_client.billingAccounts().projects().list(name=billing_id).execute()
     for project_row in project_result["projectBillingInfo"]:
@@ -13,20 +15,24 @@ def get_project_list(cloud_billing_client,billing_id):
     return project_list
 
 def get_instance_data(compute_client, project_list):
+    # Get list & details of all instances for all projects in the project list
     output = []
     for project_id in project_list:
+        # First need to get the list of zones
         zone_list = []
         zone_result = compute_client.zones().list(project=project_id).execute()
         for zone_rows in zone_result['items']:
             zone_list.append(zone_rows['name'])
         for zone_name in zone_list:
+            # For every zone for each project, get the instance details
             instances_result = compute_client.instances().list(project=project_id, zone=zone_name).execute()
-            if 'items' in instances_result:
+            if 'items' in instances_result: # Get instance details only when there are instances in the given zone
                 for instance_row in instances_result["items"]:
                     output_row = []
                     metadata_dict = {}
-                    output_row.append(project_id)
+                    output_row.append(project_id) # Project ID as the first field
                     output_row.extend([instance_row["name"],instance_row["labels"]["app-name"],instance_row["labels"]["biz-unit"],instance_row["labels"]["env-name"]])
+                    # Get nested metadat key-value entries
                     for metadata_entry in instance_row["metadata"]["items"]:
                         metadata_dict[metadata_entry['key']] = metadata_entry['value']
                     output_row.extend([metadata_dict['server-role'], metadata_dict['server-type'], metadata_dict['os-image']])
@@ -34,6 +40,7 @@ def get_instance_data(compute_client, project_list):
     return output
 
 def write_output(header_row,output):
+    # Write CSV format output to stdout
     print header_row
     for row in output:
         print ','.join(row)
